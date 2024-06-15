33faafba7931b8cb2fdc7f86e158081a26f5aa59
inderjeetsingh2003 <phenomenalinderjeet2003@gmail.com>
2024-06-15 18:40:02 +0530

    the public chat frontend

--- meetaurafrontend/src/components/CreateRoom.js
+++ meetaurafrontend/src/components/CreateRoom.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './CreateRoom.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './CreateRoom.css';
+
 
 
 export default function CreateRoom(props) {
--- meetaurafrontend/src/pages/ChatRoom.js
+++ meetaurafrontend/src/pages/ChatRoom.js
@@ -1,57 +1,95 @@
-import React, { useMemo,useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import {io} from 'socket.io-client'
+import React, { useMemo, useEffect, useState, useContext } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { io } from 'socket.io-client';
+import styled from 'styled-components';
+import { Usercontext } from '../context/Usercontext';
 
+// Styled-components for the chat
+const ChatContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+const Message = styled.div`
+  display: flex;
+  justify-content: ${props => props.isSender ? 'flex-end' : 'flex-start'};
+  padding: 10px;
+`;
+
+const MessageBubble = styled.div`
+  background-color: ${props => props.isSender ? '#FF69B4' : '#4682B4'};
+  color: #fff; /* Text color */
+  border-radius: 10px;
+  padding: 10px;
+  max-width: 60%;
+  font-weight: bold;
+`;
 
 export default function ChatRoom() {
+  const { userdetail, getuserdetails } = useContext(Usercontext);
+  const [messages, setMessages] = useState([]);
+  const [messagetosend, setMessageToSend] = useState('');
+  const { id } = useParams();
+  const socket = useMemo(() => io('http://localhost:4000'), []);
+  const navigate = useNavigate();
 
-  const[message,setmessage]=useState([])
-  const[messagetosend,setmessagetosend]=useState()
-const {id}=useParams()
-    const socket=useMemo(() => io('http://localhost:4000'), [])
-    useEffect(() => {
-      socket.on('connect',()=>
-      {
-        console.log("connected",socket.id)
-        socket.emit('setusername',{username:'rahul',roomid:id})
-        
-      })
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log("connected", socket.id);
+      socket.emit('setusername', { username: userdetail.username, roomid: id });
+    });
 
-      socket.on('userconnected',(data)=>
-      {
-        console.log(data)
-      })
-  
-      
-      socket.on('message',(message)=>
-      {
-        setmessage((prevMessages) => [...prevMessages, message]);
-      })
+    socket.on('userconnected', (data) => {
+      console.log(data);
+    });
 
+    socket.on('message', ({ message, username }) => {
+      setMessages(prevMessages => [...prevMessages, { text: message, username }]);
+    });
 
+    return () => {
+      socket.off('connect');
+      socket.off('userconnected');
+      socket.off('message');
+    };
+  }, []);
 
-      return () => {
-           socket.off('connect');
-            socket.off('userconnected');
-            socket.off('message');
-      }
-    }, [])
-    
-    const sendmessage=()=>
-      {
-        socket.emit('sendmessage', {roomid:id,message:messagetosend})
-        setmessagetosend('')
-      }
+  useEffect(() => {
+    if (!localStorage.getItem('user-token')) {
+      navigate('/');
+    }
+    getuserdetails();
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit('sendmessage', { roomid: id, message: messagetosend, username: userdetail.username });
+    setMessageToSend('');
+  };
+
   return (
     <div>
-    <h1>ChatRoom</h1>
-    <div>
-        {message.map((msg, index) => (
-            <p key={index}>{msg}</p>
-        ))}
+      <h1>ChatRoom</h1>
+      <ChatContainer>
+        {messages.map((msg, index) => {
+          const isSender = msg.username === userdetail.username;
+          return (
+            <Message key={index} isSender={isSender}>
+              <MessageBubble isSender={isSender}>
+               <div style={{color:'orange'}}> {!isSender && <small>{msg.username}</small>}</div>
+                <div>{msg.text}</div>
+              </MessageBubble>
+            </Message>
+          );
+        })}
+      </ChatContainer>
+      <input
+        onChange={(e) => setMessageToSend(e.target.value)}
+        value={messagetosend}
+      />
+      <button onClick={sendMessage} className='mx-3'>
+        Click to send
+      </button>
     </div>
-        <input onChange={(e)=>setmessagetosend(e.target.value) } value={messagetosend}/>
-        <button onClick={sendmessage} className='mx-3'> click to send</button>
-    </div>
-  )
+  );
 }
